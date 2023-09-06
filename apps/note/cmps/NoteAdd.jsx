3e02b3e891010ad9ea942560579c1b851b04719a
Omer <omernitoker@gmail.com>
2023-09-06 21:04:14 +0300

    submission2-Wednesday-21:00

---
+++ apps/note/cmps/NoteAdd.jsx
@@ -0,0 +1,6 @@
+
+export function NoteAdd() {
+    return (
+        <div>NoteAdd</div>
+    )
+}
--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -1,7 +1,45 @@
+import { NoteFilter } from "../cmps/NoteFilter.jsx"
+import { NoteList } from "../cmps/NoteList.jsx"
+import { noteService } from "../services/note.service.js"
+import { NoteAdd } from "../cmps/NoteAdd.jsx"
 
+const { useState, useEffect } = React
+const { Link } = ReactRouterDOM
 
 export function NoteIndex() {
+
+    const [notes, setNotes] = useState(null)
+    const [filterBy, setFilterBy] = useState(noteService.getDefaultFilter())
+
+    useEffect(() => {
+        noteService.query(filterBy).then(notes => {
+            setNotes(notes)
+        })
+    }, [filterBy])
+
+    function onRemoveNote(noteId) {
+        noteService.remove(noteId)
+            .then(() => {
+                setNotes(prevNotes => prevNotes.filter(note => note.id !== noteId))
+                // showSuccessMsg(`Book Removed! ${bookId}`)
+            })
+            .catch(err => {
+                console.error(err)
+                // showErrorMsg(`Problem Removing ${noteId}`)
+            })
+    }
+
+    function onSetFilterBy(filterBy) {
+        setFilterBy(prevFilter => ({ ...prevFilter, ...filterBy }))
+    }
+
+    if (!notes) return <div>Loading...</div>
+
     return (
-        
+        <section className='note-index'>
+            <NoteFilter filterBy={filterBy} onSetFilterBy={onSetFilterBy} />
+            <NoteList notes={notes} onRemoveNote={onRemoveNote} />
+            <NoteAdd />
+        </section>
     )
 }
