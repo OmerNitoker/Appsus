964d0ae96201378ede62ff5832149bc369192588
Omer <omernitoker@gmail.com>
2023-09-09 22:59:58 +0300

    fix bug

--- apps/note/cmps/Icons.jsx
+++ apps/note/cmps/Icons.jsx
@@ -3,19 +3,19 @@ export function Icons({setNoteType , noteType}) {
 
     return (
         <section className="type-icons">
-            <span className={"material-symbols-outlined" + (noteType === 'txt' ? ' selected-icon' : '')} onClick ={() => setNoteType('NoteTxt')}>
+            <span className={"material-symbols-outlined" + (noteType === 'NoteTxt' ? ' selected-icon' : '')} onClick ={() => setNoteType('NoteTxt')}>
                 text_fields
             </span>
-            <span className={"material-symbols-outlined" + (noteType === 'img' ? ' selected-icon' : '')} onClick ={() => setNoteType('NoteImg')}>
+            <span className={"material-symbols-outlined" + (noteType === 'NoteImg' ? ' selected-icon' : '')} onClick ={() => setNoteType('NoteImg')}>
                 image
             </span>
-            <span className={"material-symbols-outlined" + (noteType === 'video' ? ' selected-icon' : '')} onClick ={() => setNoteType('NoteVideo')}>
+            <span className={"material-symbols-outlined" + (noteType === 'NoteVideo' ? ' selected-icon' : '')} onClick ={() => setNoteType('NoteVideo')}>
                 videocam
             </span>
-            <span className={"material-symbols-outlined" + (noteType === 'audio' ? ' selected-icon' : '')} onClick ={() => setNoteType('NoteAudio')}>
+            {/* <span className={"material-symbols-outlined" + (noteType === 'NoteAudio' ? ' selected-icon' : '')} onClick ={() => setNoteType('NoteAudio')}>
                 mic
-            </span>
-            <span className={"material-symbols-outlined" + (noteType === 'todo' ? ' selected-icon' : '')} onClick ={() => setNoteType('Notetodos')}>
+            </span> */}
+            <span className={"material-symbols-outlined" + (noteType === 'NoteTodos' ? ' selected-icon' : '')} onClick ={() => setNoteType('NoteTodos')}>
                 format_list_bulleted
             </span>
         </section>
